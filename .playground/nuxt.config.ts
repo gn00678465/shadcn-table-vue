3da4d667b19a88e34d5031bf8c6b648602592dd0
madao <gn00678465@gmail.com>
2025-09-04 13:03:49 +0800

    chore: 更新 dependencies
    - 新增與初始化 Tailwindcss v4
    - 新增 shadcn-vue 與依賴套件，初始化 shadcn-vue
    - 更新 nuxt to 3.19.0

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,12 +1,30 @@
 import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
+  compatibilityDate: '2025-09-04',
   extends: ['..'],
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', "shadcn-nuxt"],
   eslint: {
     config: {
       // Use the generated ESLint config for lint root project as well
       rootDir: fileURLToPath(new URL('..', import.meta.url))
     }
+  },
+  css: [join(currentDir, './assets/css/tailwind.css')],
+  alias: {
+    '@': currentDir,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
   }
 })
