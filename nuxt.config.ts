d89a12fec71e810f8e2b315b529702713e000bb8
madao <gn00678465@gmail.com>
2025-09-05 15:20:01 +0800

    chore: 新增依賴 nuxt/i18n 並設定 i18n 與新增語系檔

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/i18n'],
   devtools: { enabled: true },
+  i18n: {
+    locales: [
+      { code: 'en-US', file: 'enUS.json' },
+      { code: 'zh-TW', file: 'zhTW.json' },
+    ],
+  },
 })
